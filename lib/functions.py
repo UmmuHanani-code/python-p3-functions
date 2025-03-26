950de272c3bcaed3f4d0f1dec8efb9414f90cf3e
Nuria Mohamed Hussein <nuria.hussein@student.moringaschool.com>
2025-03-26 15:55:28 +0300

    commit python functions first lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Nuria")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("Hamza")
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    return sum
+
+add(8, 8)
 
 def halve(number):
-    pass
+    return number / 2
+
+halve(10)
